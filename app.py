443b56b4b70e37eecd8773ac8b42568dc36bb004
ktvdi <kom.tvdigitalid@gmail.com>
2025-09-14 16:36:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+def home():
+    return render_template('index.html')
 
 @app.route("/about")
 def about():
